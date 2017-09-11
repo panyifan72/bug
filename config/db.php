d93e5d798ff8ac24fca7fe48c4941e5526ab0643
panyifan72 <5890695@qq.com>
2017-09-11 10:33:11 +0800

    Revert "测试"
    This reverts commit ab8334d95d8ab1ad0023ea11dae4aa474af82efc.

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=test',
-    'username' => 'root',
-    'password' => '123456',
-    'charset' => 'utf8',
-];
--- modules/bug/views/web/index.php
+++
@@ -1,5 +0,0 @@
-<form action="/bug/web/show" method="post">
-    链接:<input type="text" name="url" value="">
-    控制器:<input type="text" name="controller" value="">
-    <input type="submit" value="提交" />
-</form>
